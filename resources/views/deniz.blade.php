917c5960e108f859e369db62186e5c7911205e2f
dangeralexisis <dangeralexisis@gmail.com>
2024-11-07 12:07:20 +0000

    cf ip client bypass fix

--- resources/views/deniz.blade.php
+++ resources/views/deniz.blade.php
@@ -76,6 +76,24 @@ fbq('track', 'PageView');
 src="https://www.facebook.com/tr?id=1095347845476726&ev=PageView&noscript=1"
 /></noscript>
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(98844621, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/98844621" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 <body class="login" style="">
    
   <app ng-version="8.2.14">
