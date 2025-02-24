87e45ed77a6a98db6ffb91dbb94cca75bf0313cd
dangerdevelop <dangeralexisis@gmail.com>
2025-02-25 01:06:01 +0300

    check fbq

--- resources/views/wordpress.blade.php
+++ resources/views/wordpress.blade.php
@@ -113,11 +113,11 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1615367679196190');
+fbq('init', '1386797192700543');
 fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1615367679196190&ev=PageView&noscript=1"
+src="https://www.facebook.com/tr?id=1386797192700543&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Meta Pixel Code -->
 @endif
