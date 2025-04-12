32f7993d7c9e09490a598eedb58b4bdff633b823
dangerdevelop <dangeralexisis@gmail.com>
2025-04-12 12:13:08 +0300

    piksel change

--- resources/views/i.blade.php
+++ resources/views/i.blade.php
@@ -133,18 +133,12 @@
     t.src=v;s=b.getElementsByTagName(e)[0];
     s.parentNode.insertBefore(t,s)}(window, document,'script',
     'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1386797192700543');
+    fbq('init', '1234782915317881');
     fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=1386797192700543&ev=PageView&noscript=1"
-    /></noscript>
-    <script type="text/javascript">
-        (function() {
-            window["loaderConfig"] = "/TSPD/?type=20";
-        })();
-    </script>
-
+  </script>
+  <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=1234782915317881&ev=PageView&noscript=1"
+  /></noscript>
     <!-- <script type="text/javascript" src="/TSPD/?type=18"></script> -->
 
 
--- resources/views/wordpress.blade.php
+++ resources/views/wordpress.blade.php
@@ -121,9 +121,6 @@
   /></noscript>
   <!-- End Meta Pixel Code -->
 @endif
-
-<!-- Meta Pixel Code -->
-<!-- Meta Pixel Code -->
 <script>
     !function(f,b,e,v,n,t,s)
     {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -133,13 +130,12 @@
     t.src=v;s=b.getElementsByTagName(e)[0];
     s.parentNode.insertBefore(t,s)}(window, document,'script',
     'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1221223196102589');
+    fbq('init', '1234782915317881');
     fbq('track', 'PageView');
   </script>
   <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=1221223196102589&ev=PageView&noscript=1"
+    src="https://www.facebook.com/tr?id=1234782915317881&ev=PageView&noscript=1"
   /></noscript>
-  <!-- End Meta Pixel Code -->
 </head>
 
 <body
